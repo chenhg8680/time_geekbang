c2059031f26aceb6ecaa486c7bd3ccc2d8373351
陈红光 <chg@meitu.com>
2021-08-08 16:30:57 +0800

    edit

---
+++ week1/HelloClassLoader.java
@@ -0,0 +1,76 @@
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+//import java.util.Base64;
+
+public class HelloClassLoader extends ClassLoader{
+	public static void main(String[] args) throws Exception {
+        // 相关参数
+        final String className = "Hello";
+        final String methodName = "hello";
+        // 创建类加载器
+        ClassLoader classLoader = new XlassLoader();
+        // 加载相应的类
+        Class<?> clazz = classLoader.loadClass(className);
+        // 看看里面有些什么方法
+        for (Method m : clazz.getDeclaredMethods()) {
+            System.out.println(clazz.getSimpleName() + "." + m.getName());
+        }
+        // 创建对象
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+        // 调用实例方法
+        Method method = clazz.getMethod(methodName);
+        method.invoke(instance);
+    }
+
+	@Override
+	public Class<?> findClass(String name) throws ClassNotFoundException{
+		String helloBase64 = "yv66vgAAADQAHAoABgAOCQAPABAIABEKABIAEwcAFAcAFQEABjxpbml0PgEAAygpVgEABENvZGUBAA9MaW5lTnVtYmVyVGFibGUBAAg8Y2xpbml0PgEAClNvdXJjZUZpbGUBAApIZWxsby5qYXZhDAAHAAgHABYMABcAGAEAFkhlbGxvIENsYXNzIEluaXRpYWxpemQHABkMABoAGwEABUhlbGxvAQAQamF2YS9sYW5nL09iamVjdAEAEGphdmEvbGFuZy9TeXN0ZW0BAANvdXQBABVMamF2YS9pby9QcmludFN0cmVhbTsBABNqYXZhL2lvL1ByaW50U3RyZWFtAQAHcHJpbnRsbgEAFShMamF2YS9sYW5nL1N0cmluZzspVgAhAAUABgAAAAAAAgABAAcACAABAAkAAAAdAAEAAQAAAAUqtwABsQAAAAEACgAAAAYAAQAAAAEACAALAAgAAQAJAAAAJQACAAAAAAAJsgACEgO2AASxAAAAAQAKAAAACgACAAAAAwAIAAQAAQAMAAAAAgAN";
+
+		byte[] bytes = decode(helloBase64);
+
+		return defineClass(name,bytes,0,bytes.length);
+	}
+
+	@Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        // 如果支持包名, 则需要进行路径转换
+        String resourcePath = name.replace(".", "/");
+        // 文件后缀
+        final String suffix = ".xlass";
+        // 获取输入流
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(resourcePath + suffix);
+        try {
+            // 读取数据
+            int length = inputStream.available();
+            byte[] byteArray = new byte[length];
+            inputStream.read(byteArray);
+            // 转换
+            byte[] classBytes = decode(byteArray);
+            // 通知底层定义这个类
+            return defineClass(name, classBytes, 0, classBytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException(name, e);
+        } finally {
+            close(inputStream);
+        }
+    }
+
+	public byte[] decode(String base64){
+		return Base64.getDecoder().decode(base64);
+	}
+
+	// 关闭
+    private static void close(Closeable res) {
+        if (null != res) {
+            try {
+                res.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
+
